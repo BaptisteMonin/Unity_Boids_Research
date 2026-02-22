040db21a4703661d19604a90da0d65ad2018893c
Baptiste Monin <baptistemonin.profl@gmail.com>
2026-02-22 18:18:26 +0100

    [Add] Cohesion algorithm

--- Boids_Simulation/Assets/Scripts/Core/BoidLogic.cs
+++ Boids_Simulation/Assets/Scripts/Core/BoidLogic.cs
@@ -3,9 +3,9 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Rendering;
 
-public class BoidLogic : MonoBehaviour
+public class BoidLogic
 {
-    Vector3 SeparationAlgorithm(BoidAgent self, List<BoidAgent> otherAgents, float protectedRange, float avoidFactor)
+    public static Vector3 SeparationAlgorithm(BoidAgent self, List<BoidAgent> otherAgents, float protectedRange, float avoidFactor)
     {
         float CloseX = 0f;
         float CloseY = 0f;
@@ -25,7 +25,7 @@ public class BoidLogic : MonoBehaviour
         return new Vector3(DirectX, DirectY, 0);
     }
 
-    Vector3 AlignmentAlgorithm(BoidAgent self, List<BoidAgent> otherAgents, float visualRange, float matchingFactor)
+    public static Vector3 AlignmentAlgorithm(BoidAgent self, List<BoidAgent> otherAgents, float visualRange, float matchingFactor)
     {
         float DirectAvgX = 0f;
         float DirectAvgY = 0f;
@@ -52,8 +52,30 @@ public class BoidLogic : MonoBehaviour
         return new Vector3(DirectX, DirectY, 0);
     }
 
-    Vector3 CohesionAlgorithm()
+    public static Vector3 CohesionAlgorithm(BoidAgent self, List<BoidAgent> otherAgents, float visualRange, float centeringFactor)
     {
-        return new Vector3(0, 0, 0);
+        float PosAvgX = 0f;
+        float PosAvgY = 0f;
+        int neighbourNbr = 0;
+
+        foreach (BoidAgent agent in otherAgents)
+        {
+            if (agent == self) continue;
+            float distance = Vector3.Distance(self.transform.position, agent.transform.position);
+            if (distance <= visualRange)
+            {
+                PosAvgX += agent.transform.position.x;
+                PosAvgY += agent.transform.position.y;
+                neighbourNbr += 1;
+            }
+        }
+        if (neighbourNbr > 0)
+        {
+            PosAvgX = PosAvgX / neighbourNbr;
+            PosAvgY = PosAvgY / neighbourNbr;
+        }
+        float DirectX = (PosAvgX - self.transform.position.x) * centeringFactor;
+        float DirectY = (PosAvgY - self.transform.position.y) * centeringFactor;
+        return new Vector3(DirectX, DirectY, 0);
     }
 }
